5ad26ac04bacdc3301c6d100b5ae212570ffce53
it63070003 <63070003@it.kmitl.ac.th>
2023-03-31 11:29:58 +0700

    update 31

--- gameJS.js
+++ gameJS.js
@@ -3,7 +3,7 @@ var playerShape = 'O';
 var enemyShape = 'X';
 var win = false;
 var winner = '';
-var blocks = document.querySelectorAll('.table-block');
+const blocks = document.querySelectorAll('.table-block');
 var turnObject = document.getElementById('turn');
 var game_continue = true;
 var usingItem = false;
@@ -13,7 +13,16 @@ var playerAction = {};
 var enemyMove = '';
 var enemyAction = {};
 
+var playerRevolverPart = 0;
+var playerRevolverBullet = 6;
+var enemyRevolverPart = 0;
+var enemyRevolverBullet = 6;
+var revolverWinner = '';
+
 var trappedBlock = '';
+var spyBlock = '';
+
+var selectedBull = '';
 
 for (block of blocks) {
     let blockId = block.id;
@@ -27,9 +36,9 @@ for (block of blocks) {
             return false;
         }
         let shapeValue = event.currentTarget.getAttribute('data-shape');
-        let action = event.currentTarget.getAttribute('data-action'); // "E", "gun-shoot", "trap-place", "spy-select", "spy-guess" ,"bull-select", "bull-move"
-        
-        if(shapeValue == 'E' && event.currentTarget.id == trappedBlock && !usingItem){
+        let action = event.currentTarget.getAttribute('data-action'); // "E", "gun-shoot", "trap-place", "spy-select", "spy-guess" ,"bull-select", "bull-move, revolver"
+
+        if (shapeValue == 'E' && event.currentTarget.id == trappedBlock && !usingItem) {
             console.log("TRAPPED!!!!!");
             let trapText = `<h1 class="ml4">
                             Trapped
@@ -38,9 +47,27 @@ for (block of blocks) {
             //event.currentTarget.appendChild(trapTextElem);
             document.getElementById('table').appendChild(trapTextElem);
 
-            
+            trappedBlock = '';
             randomItem() ? usingItem = true : endTurn();
-        }else if (shapeValue == 'E' && action == 'E' && !usingItem) {
+        } else if (shapeValue == playerShape && action == "spy-guess" && !usingItem) {
+            if (event.currentTarget.id == spyBlock) {
+                console.log("Spy guess Sucess");
+            }
+            else {
+                console.log("Spy kill your shape And Disguised");
+                changeShape(spyBlock, enemyShape);
+                if (checkResult()) {
+                    return true;
+                }
+            }
+
+            for (block of blocks) {
+                block.setAttribute('data-action', 'E');
+                block.style.backgroundColor = 'white';
+            }
+            spyBlock = '';
+
+        } else if (shapeValue == 'E' && action == 'E' && !usingItem && spyBlock == '') {
             // 4. Modify the code here to check whether the clicking block is avialable.
             changeShape(event.currentTarget.id, playerShape);
 
@@ -65,6 +92,25 @@ for (block of blocks) {
             updateActionToEnemy(playerAction);
 
             endTurn();
+        } else if (shapeValue == enemyShape && action == "spy-select") {
+            let spyPos = useSpy(this);
+            playerAction = { "item": 'spy', "pos": spyPos };
+            updateActionToEnemy(playerAction);
+
+            endTurn();
+        } else if (shapeValue == enemyShape && action == "bull-select") {
+            boardClean();
+            this.style.backgroundColor = 'green';
+            selectedBull = checkBullMove(this);
+        } else if (shapeValue == 'E' && action == "bull-move") {
+            let bullPos = useBull(this);
+            console.log("Moving bull");
+            playerAction = { "item": 'bull', "pos": bullPos };
+            updateActionToEnemy(playerAction);
+            if (checkResult()) {
+                return true;
+            }
+            endTurn();
         }
     });
 }
@@ -83,10 +129,10 @@ function changeShape(pos, shape) {
 function randomItem() {
     console.log('randomitem!');
     //let itemIndex = Math.floor(Math.random() * 4);
-    let itemIndex = 1; //!!!!!!!!!Test item
+    let itemIndex = 4; //!!!!!!!!!Test item
 
-    let itemsString = ['gun', 'trap', 'spy', 'bull']
-    let itemsImg = ['images/gun_icon.webp', 'images/trap_icon.webp', 'images/spy_icon.webp', 'images/bull_icon.webp']
+    let itemsString = ['gun', 'trap', 'spy', 'bull', 'revolver']
+    let itemsImg = ['images/gun_icon.webp', 'images/trap_icon.webp', 'images/spy_icon.webp', 'images/bull_icon.webp', 'images/gun_icon.webp']
     document.getElementById('currentItem').setAttribute('src', itemsImg[itemIndex]);
     return useItem(itemsString[itemIndex]);
 }
@@ -98,6 +144,21 @@ function useItem(item) {
         case 'trap':
             // code block
             return checkTrap();
+            break;
+        case 'spy':
+            return checkSpy();
+            break;
+        case 'bull':
+            return checkBullSelect();
+            break;
+        case 'revolver':
+            if (playerRevolverPart == 3) {
+                useRevolver();
+            }
+            else {
+                playerRevolverPart += 1;
+            }
+            return false;
             break;
         default:
         // code block
@@ -146,6 +207,80 @@ function checkTrap() {
     return usable;
 }
 
+function checkSpy() {
+    let usable = false;
+    for (block of blocks) {
+        if (block.getAttribute('data-shape') == enemyShape) {
+            block.setAttribute('data-action', 'spy-select');
+            block.style.backgroundColor = 'orange';
+            usable = true;
+        }
+    }
+    return usable;
+}
+
+function checkBullSelect() {
+    let usable = false;
+    for (block of blocks) {
+        if (block.getAttribute('data-shape') == enemyShape) {
+            // block = B2  Check B1 A2 B3 C2
+            let blockPos = block.getAttribute('id')
+            let upLeftBlock = document.getElementById(String.fromCharCode(blockPos[0].charCodeAt(0) - 1) + (parseInt(blockPos[1]) - 1).toString());
+            let upRightBlock = document.getElementById(String.fromCharCode(blockPos[0].charCodeAt(0) - 1) + (parseInt(blockPos[1]) + 1).toString());
+            let downLeftBlock = document.getElementById(String.fromCharCode(blockPos[0].charCodeAt(0) + 1) + (parseInt(blockPos[1]) - 1).toString());
+            let downRightBlock = document.getElementById(String.fromCharCode(blockPos[0].charCodeAt(0) + 1) + (parseInt(blockPos[1]) + 1).toString());
+
+            checkBlock(upLeftBlock, block);
+            checkBlock(upRightBlock, block);
+            checkBlock(downLeftBlock, block);
+            checkBlock(downRightBlock, block);
+        }
+    }
+    function checkBlock(thatBlock, block) {
+        try {
+            if (thatBlock.getAttribute('data-shape') == 'E') {
+                block.setAttribute('data-action', 'bull-select');
+                block.style.backgroundColor = 'green';
+                usable = true;
+            }
+        } catch (err) {
+        }
+    }
+    return usable;
+}
+
+function checkBullMove(block) {
+    if (block.getAttribute('data-shape') == enemyShape) {
+        // block = B2  Check B1 A2 B3 C2
+        let blockPos = block.getAttribute('id')
+        let upLeftBlock = document.getElementById(String.fromCharCode(blockPos[0].charCodeAt(0) - 1) + (parseInt(blockPos[1]) - 1).toString());
+        let upRightBlock = document.getElementById(String.fromCharCode(blockPos[0].charCodeAt(0) - 1) + (parseInt(blockPos[1]) + 1).toString());
+        let downLeftBlock = document.getElementById(String.fromCharCode(blockPos[0].charCodeAt(0) + 1) + (parseInt(blockPos[1]) - 1).toString());
+        let downRightBlock = document.getElementById(String.fromCharCode(blockPos[0].charCodeAt(0) + 1) + (parseInt(blockPos[1]) + 1).toString());
+
+        checkBlock(upLeftBlock);
+        checkBlock(upRightBlock);
+        checkBlock(downLeftBlock);
+        checkBlock(downRightBlock);
+    }
+    function checkBlock(thatBlock) {
+        try {
+            if (thatBlock.getAttribute('data-shape') == 'E') {
+                thatBlock.setAttribute('data-action', 'bull-move');
+                thatBlock.style.backgroundColor = 'yellow';
+            }
+        } catch (err) {
+        }
+    }
+    return block.id;
+}
+
+function checkRevolver() {
+    if (playerRevolverPart >= 3) {
+        useRevolver();
+    }
+}
+
 function useGun(shape) {
     let shapePos = shape.id;
     destroyShape(shapePos);
@@ -157,13 +292,55 @@ function useTrap(shape) {
     return shapePos;
 }
 
+function useSpy(shape) {
+    let shapePos = shape.id;
+    return shapePos;
+}
+
+function useBull(shape) {
+    let selectPos = selectedBull;
+    let moveToPos = shape.id;
+
+    destroyShape(selectPos);
+    changeShape(moveToPos, enemyShape);
+
+    return [selectPos, moveToPos];
+}
+
+function useRevolver() {
+    let randomNum = getRandomInt(playerRevolverBullet);
+    let shotted = false;
+    if (randomNum == playerRevolverBullet) {
+        revolverWinner = playerShape;
+        shotted = true;
+    }
+    else {
+        playerRevolverBullet -= 1;
+    }
+
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max) + 1;
+    }
+
+    playerAction = { "item": 'revolver', "pos": shotted };
+    updateActionToEnemy(playerAction);
+    console.log('parts: ', playerRevolverPart, 'bullet', playerRevolverBullet);
+}
+
 function destroyShape(shapePos) {
     changeShape(shapePos, 'E')
 
 }
 
+function boardClean() {
+    for (block of blocks) {
+        block.setAttribute('data-action', 'E');
+        block.style.backgroundColor = 'white';
+    }
+}
+
 function checkResult() {
-    console.log('Check!');
+    let winShape = '';
     let winning_condition = false;
     let draw_condition = false;
 
@@ -171,6 +348,9 @@ function checkResult() {
         winning_condition = true;
     } else if (checkDrawCondition()) {
         draw_condition = true;
+    } else if (revolverWinner != '') {
+        winning_condition = true;
+        winShape = revolverWinner;
     }
 
     function checkWinCondition() {
@@ -182,7 +362,7 @@ function checkResult() {
             let el4 = document.getElementById(alpha + "3").getAttribute('data-shape');
 
             if ((el1 == el2 && el2 == el3 && el3 == el4) && el1 != 'E') {
-
+                winShape = el1;
                 return true;
             }
         }
@@ -194,7 +374,7 @@ function checkResult() {
             let el4 = document.getElementById('D' + numer).getAttribute('data-shape');
 
             if ((el1 == el2 && el2 == el3 && el3 == el4) && el1 != 'E') {
-
+                winShape = el1;
                 return true;
             }
         }
@@ -204,6 +384,7 @@ function checkResult() {
         el3 = document.getElementById('C2').getAttribute('data-shape');
         el4 = document.getElementById('D3').getAttribute('data-shape');
         if ((el1 == el2 && el2 == el3 && el3 == el4) && el1 != 'E') {
+            winShape = el1;
             return true;
         }
 
@@ -212,6 +393,7 @@ function checkResult() {
         el3 = document.getElementById('C1').getAttribute('data-shape');
         el4 = document.getElementById('D0').getAttribute('data-shape');
         if ((el1 == el2 && el2 == el3 && el3 == el4) && el1 != 'E') {
+            winShape = el1;
             return true;
         }
 
@@ -229,7 +411,7 @@ function checkResult() {
 
     if (winning_condition) {
         //Game end and someone wins the game
-        winner = turn;
+        winner = winShape;
         turnObject.innerHTML = "Game win by " + winner;
         game_continue = false;
         return true;
@@ -247,16 +429,26 @@ function endTurn() {
     usingItem = false;
 
     //reset data-action, BG
-    for (block of blocks) {
-        block.setAttribute('data-action', 'E');
-        block.style.backgroundColor = 'white';
-    }
+    boardClean();
+
+    checkRevolver();
+    checkResult();
     //startTurn();//อีกฝ่าย
 }
 
 function startTurn() {
     turn = turn === 'O' ? 'X' : 'O';
+    turnObject.innerHTML = "Turn: " + turn;
 
+    for (block of blocks) {
+        if (spyBlock != '' && block.getAttribute('data-shape') == playerShape) {
+            block.setAttribute('data-action', 'spy-guess');
+            block.style.backgroundColor = 'purple';
+        }
+    }
+    if (checkResult()) {
+        return true;
+    }
 }
 
 function updateMoveToEnemy(playerMove) {
@@ -281,12 +473,25 @@ function updateEnemyAction() {
     let enemyActionPos = document.getElementById("enemyActionPos").value;
     enemyAction = { "item": enemyItem, "pos": enemyActionPos };
 
+    if (enemyItem == 'bull') {
+        enemyAction = { "item": enemyItem, "pos": ['A0', 'B1'] }
+    }
+
     switch (enemyAction["item"]) {
         case 'gun':
             enemyGun(enemyAction["pos"]);
             break;
         case 'trap':
             enemyTrap(enemyAction["pos"]);
+            break;
+        case 'spy':
+            enemySpy(enemyAction["pos"]);
+            break;
+        case 'bull':
+            enemyBull(enemyAction["pos"]);
+            break;
+        case 'revolver':
+            enemyRevolver(enemyAction["pos"]);
             break;
         default:
         // code block
@@ -297,9 +502,29 @@ function updateEnemyAction() {
     function enemyGun(pos) {
         destroyShape(pos);
     }
-    function enemyTrap(pos){
+    function enemyTrap(pos) {
         trappedBlock = pos;
     }
+    function enemySpy(pos) {
+        spyBlock = pos;
+    }
+    function enemyBull(pos) {
+        destroyShape(pos[0])
+        changeShape(pos[1], playerShape)
+    }
+    function enemyRevolver(shotted) {
+        //enemyShoot animation
+        if (enemyRevolverPart == 3) {
+            //animation
+        }
+        else {
+            enemyRevolverPart += 1;
+            //add part animation
+        }
+        revolverWinner = enemyShape;
+        checkResult();
+    }
+
 }
 
 // animation
